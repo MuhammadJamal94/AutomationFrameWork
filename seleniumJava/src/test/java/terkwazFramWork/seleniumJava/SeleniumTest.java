862b345249188061d57d594865b664d3d162ebe4
Muhammad Jamal <muhammadsolais@gmail.com>
2020-05-25 08:53:06 +0200

    Initial version of SeleniumFrameWork

---
+++ seleniumJava/src/test/java/terkwazFramWork/seleniumJava/SeleniumTest.java
@@ -0,0 +1,21 @@
+package terkwazFramWork.seleniumJava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void browserAutomation()
+	{
+		
+	}
+	
+	@Test
+	public void elementsUI()
+	{
+		
+	}
+	
+	
+
+}
